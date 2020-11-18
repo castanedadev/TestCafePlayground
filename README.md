53281a0a53fd046c1bc6b5abc6a21741cca7234f
Rick C <rickcastaneda@outlook.com>
2020-11-18 00:12:40 -0600

    Expand tests & include new fixture

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
+Repository created to play around with Testcafe automation framework (https://github.com/DevExpress/testcafe)
+
+1. `Node.js` and `npm` must be installed prior
+2. `npm install -g testcafe`
+2. Develop test scenarios in JS or TS 
+
 React Selectors Documentation: 
 - [https://dev.to/pluralsight/react-specific-selectors-in-testcafe-22k5]
 - [https://github.com/DevExpress/testcafe-react-selectors]
 
 
 How to Run:
-WSL =>  testcafe --ports 63305,63305 --live remote tests/test_home.ts  
+WSL =>  testcafe --ports 63305,63305 --live remote tests/test_home.ts  ' 
--- src/pages/home_page.ts
+++ src/pages/home_page.ts
@@ -2,21 +2,21 @@ import { ReactSelector } from 'testcafe-react-selectors';
 import BasePage from './base_page';
 
 export default class HomePage extends BasePage {
-    // Following alphabetic order to easily find selectors by name
-    public appLogo: Selector;
-    public detailsButton: Selector;
-    public mainTitle: Selector;
-    public startButton: Selector;
-    public welcomeBlock: Selector;
-    public welcomeDescription: Selector;
+  // Following alphabetic order to easily find selectors by name
+  public appLogo: Selector;
+  public detailsButton: Selector;
+  public mainTitle: Selector;
+  public startButton: Selector;
+  public welcomeBlock: Selector;
+  public welcomeDescription: Selector;
 
-    constructor() {
-        super();
-        this.appLogo = ReactSelector('img').withProps({ 'alt': 'logo', 'className': 'welcome-logo' });
-        this.detailsButton = ReactSelector('button').withText(/Details/i);
-        this.mainTitle = ReactSelector('h3').withProps({'className': 'welcome-intro'});
-        this.startButton = ReactSelector('button').withText(/Start/i);
-        this.welcomeBlock = ReactSelector('div').withProps({ 'className': 'welcome-block' });
-        this.welcomeDescription = ReactSelector('p').withProps({ 'className': 'welcome-description'});
-    }
+  constructor() {
+    super();
+    this.appLogo = ReactSelector('img').withProps({ alt: 'logo', className: 'welcome-logo' });
+    this.detailsButton = ReactSelector('button').withText(/Details/i);
+    this.mainTitle = ReactSelector('h3').withProps({ className: 'welcome-intro' });
+    this.startButton = ReactSelector('button').withText(/Start/i);
+    this.welcomeBlock = ReactSelector('div').withProps({ className: 'welcome-block' });
+    this.welcomeDescription = ReactSelector('p').withProps({ className: 'welcome-description' });
+  }
 }
